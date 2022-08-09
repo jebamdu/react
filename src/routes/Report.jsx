1215af63b4c7c45e89e3820d286bbaec415595be
jeba <jebastinmdu01@gmail.com>
2022-08-09 13:17:44 +0530

    0908

--- src/routes/Report.jsx
+++ src/routes/Report.jsx
@@ -8,6 +8,13 @@ const Report = () => {
   const [showbatch, setshowbatch] = useState([]);
   const [level, setlevel] = useState([]);
   const [batch_id, setbatch_id] = useState();
+  const [show_exact_mark, setshow_exact_mark] = useState(false);
+  const [idarray, setidarray] = useState([]);
+  const [c_lev, setc_lev] = useState();
+  const [extractdata, setextractdata] = useState([]);
+  const extractDataRef = useRef([]);
+  const [categoryname, setcategoryname] = useState([]);
+  // const [data, setdata] = useState(0);
 
   const [pop, setpop] = useState("");
 
@@ -37,12 +44,14 @@ const Report = () => {
   const currentLevel = useRef(null);
   const getlevel = async (id) => {
     currentLevel.current = id;
+    setc_lev(id);
     if (batch_id) {
       const values = await axios.get("/stdtable", {
         params: { levelid: id, batchid: batch_id },
       });
       console.log(values);
-      setshowbatch(values.data);
+      setshowbatch(values.data.data[0]);
+      setidarray(values.data.data[1]);
     } else {
       alert("Please select the batch");
     }
@@ -51,6 +60,34 @@ const Report = () => {
     // console.log(e);
     setpop(email);
   }
+  function extract(num) {
+    if (num != {}) {
+      return num;
+    }
+  }
+  const extract_stud_detail_mark = async () => {
+    console.log(c_lev, "current level");
+    const extract_stud_detail = await axios.post("/extract_stud_detail", {
+      id: c_lev,
+      cat_id: idarray,
+      email: showbatch.map((i) => i["email"]),
+    });
+    console.log(extract_stud_detail.data);
+    setextractdata(extract_stud_detail.data[0][0]);
+    extractDataRef.current = extract_stud_detail.data[0];
+    setcategoryname(extract_stud_detail.data[1]);
+  };
+
+  const exact_mar_fun = () => {
+    setshow_exact_mark(true);
+    console.log(idarray, "id array ");
+    if (!c_lev && !idarray) {
+      return alert("Please check batchname and survey's name");
+    }
+
+    extract_stud_detail_mark();
+  };
+
   return (
     // <div className="container">
     <div className="containerBody">
@@ -80,12 +117,56 @@ const Report = () => {
           // </form>
         }
       </div>
-      <div className="wrapper">
+      <div className="wrapper" >
         <div className="mainContainer">
           <div className="content">
-          <button
+
+          {show_exact_mark&& <button
               className="btn primary"
-              style={{ width: "max-content" ,position:"absolute",right:"22%",top:"1%"}}
+              style={{
+                width: "max-content",
+                position: "absolute",
+                right: "47%",
+                top: "1%",
+              }}
+              onClick={() => (setshow_exact_mark(false))}
+            >
+              back
+            </button> }
+          {show_exact_mark &&
+            <select
+              className="listbox_stdrep"
+              style={{ right: "550px" }}
+              onChange={(e) =>
+                setextractdata(extractDataRef.current[e.target.value])
+              }
+            >
+              {categoryname.map((e, i) => (
+                <option value={i} key={i}>
+                  {e}
+                </option>
+              ))}
+            </select>}
+            <button
+              className="btn primary"
+              style={{
+                width: "max-content",
+                position: "absolute",
+                right: "32%",
+                top: "1%",
+              }}
+              onClick={() => exact_mar_fun()}
+            >
+              View more
+            </button>
+            <button
+              className="btn primary"
+              style={{
+                width: "max-content",
+                position: "absolute",
+                right: "22%",
+                top: "1%",
+              }}
               onClick={() => {
                 console.log("from export");
                 let csv = "";
@@ -136,11 +217,12 @@ const Report = () => {
                 </option>
               ))}
             </select>
-            
-            
-            
-            <br/>
-            <Table className="table" emailFn={emailFn} data={showbatch} />
+
+            <br />
+            {!show_exact_mark && (
+              <Table className="table" emailFn={emailFn} data={showbatch} />
+            )}
+            {show_exact_mark && <Table className="table" data={extractdata} />}
             {pop && (
               <PopChart
                 email={pop}
@@ -169,11 +251,8 @@ const Report = () => {
               )
               // </Link>
             )} */}
-            <br/>
-            
+            <br />
           </div>
-          
-          
         </div>
       </div>
     </div>
